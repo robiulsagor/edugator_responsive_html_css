07c5405a8ffe812d9c6f852828257caa9442a2b3
robiulsagor <robiul100.me@gmail.com>
2023-11-20 18:55:09 +0600

    faqs done

--- script.js
+++ script.js
@@ -14,7 +14,14 @@ window.addEventListener("scroll", () => {
 const faqs = document.querySelectorAll(".faq")
 faqs.forEach(faq => {
     faq.addEventListener("click", () => {
-        console.log("tyr");
         faq.classList.toggle("open")
+
+        const icon = faq.querySelector(".faq__icon i")
+        if (icon.className === "uil uil-plus") {
+            icon.className = "uil uil-minus"
+        } else {
+            icon.className = "uil uil-plus"
+        }
+
     })
 })
